15c2398021ebc43e9ee685419087bbaca69e6790
Ian <iwburns8@gmail.com>
2018-03-27 00:09:44 -0500

    first swing at Tree and NodeMut structs

--- src/node/mod.rs
+++ src/node/mod.rs
@@ -1,3 +1,4 @@
+use tree::Tree;
 use tree::core::NodeId;
 
 pub struct Node<T> {
@@ -70,3 +71,50 @@ impl<T> Node<T> {
         self.last_child = last_child;
     }
 }
+
+pub struct NodeMut<'a, T: 'a> {
+    pub(crate) node_id: NodeId,
+    pub(crate) tree: &'a mut Tree<T>,
+}
+
+impl<'a, T: 'a> NodeMut<'a, T> {
+    pub(crate) fn new(node_id: NodeId, tree: &'a mut Tree<T>) -> NodeMut<'a, T> {
+        NodeMut {
+            node_id,
+            tree,
+        }
+    }
+
+    pub fn parent(&mut self) -> Option<NodeMut<T>> {
+        // todo: fix when non-lexical-lifetimes comes out
+        let parent_id;
+        {
+            let node = unsafe {
+                self.tree.get_unchecked(&self.node_id)
+            };
+            parent_id = node.parent.clone()?;
+        }
+        let parent = unsafe {
+            self.tree.get_unchecked_mut(&parent_id)
+        };
+        Some(parent)
+    }
+
+    pub fn append() {
+        unimplemented!()
+    }
+
+    pub fn prepend() {
+        unimplemented!()
+    }
+
+    pub fn remove_first() {
+        unimplemented!()
+    }
+
+    pub fn remove_last() {
+        unimplemented!()
+    }
+}
+
+
--- src/tree/mod.rs
+++ src/tree/mod.rs
@@ -1,3 +1,65 @@
 
 pub mod core;
 pub mod error;
+
+use node::Node;
+use node::NodeMut;
+use self::core::CoreTree;
+use self::core::NodeId;
+use self::error::NodeIdError;
+
+pub struct Tree<T> {
+    root_id: Option<NodeId>,
+    core_tree: CoreTree<T>,
+}
+
+// todo: make a builder for this
+
+impl<T> Tree<T> {
+    pub fn new() -> Tree<T> {
+        Tree {
+            root_id: None,
+            core_tree: CoreTree::new(0),
+        }
+    }
+
+    pub fn root(&self) -> Option<&Node<T>> {
+        self.root_id
+            .clone()
+            .map(|id| {
+                self.core_tree.get(&id).expect("could not get root node from core_tree!")
+            })
+    }
+
+    pub fn root_mut(&mut self) -> Option<NodeMut<T>> {
+        self.root_id
+            .clone()
+            .map(move |id| self.new_node_mut(id))
+    }
+
+    pub fn get(&self, node_id: &NodeId) -> Result<&Node<T>, NodeIdError> {
+        self.core_tree.get(node_id)
+    }
+
+    pub fn get_mut(&mut self, node_id: &NodeId) -> Result<NodeMut<T>, NodeIdError> {
+        let _ = self.core_tree.get(node_id)?;
+        Ok(self.new_node_mut(node_id.clone()))
+    }
+
+    pub unsafe fn get_unchecked(&self, node_id: &NodeId) -> &Node<T> {
+        self.core_tree.get_unchecked(node_id)
+    }
+
+    pub unsafe fn get_unchecked_mut(&mut self, node_id: &NodeId) -> NodeMut<T> {
+        self.new_node_mut(node_id.clone())
+    }
+
+    pub(crate) fn new_node_mut(&mut self, node_id: NodeId) -> NodeMut<T> {
+        NodeMut {
+            node_id,
+            tree: self
+        }
+    }
+}
+
+
