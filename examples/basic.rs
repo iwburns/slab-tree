01e8c21b305bc5e405193df960dd4ff6b8e5b6ca
Ian <iwburns8@gmail.com>
2019-08-03 00:23:24 -0500

    update basic example to match re-worked Tree(Builder)

--- examples/basic.rs
+++ examples/basic.rs
@@ -11,13 +11,10 @@ fn main() {
     //              |
     //            "cool"
 
-    let mut tree = Tree::new("hello");
-    let root_id = tree.root_id();
-    let mut hello = tree.get_mut(root_id).ok().unwrap();
+    let mut tree = TreeBuilder::new().with_root("hello").build();
+    let root_id = tree.root_id().expect("root doesn't exist?");
+    let mut hello = tree.get_mut(root_id).unwrap();
 
     hello.append("world");
-    hello
-        .append("trees")
-        .append("are")
-        .append("cool");
+    hello.append("trees").append("are").append("cool");
 }
