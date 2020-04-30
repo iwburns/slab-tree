2a63e73c60f1e7bfe5e088f75e515c7224fa0ff8
Chris Barnes <barnesc@janelia.hhmi.org>
2020-04-30 12:54:28 +0100

    Fix clippy lints

--- src/lib.rs
+++ src/lib.rs
@@ -33,26 +33,23 @@
 //! ```
 //! use slab_tree::*;
 //!
-//! fn main() {
+//! //      "hello"
+//! //        / \
+//! // "world"   "trees"
+//! //              |
+//! //            "are"
+//! //              |
+//! //            "cool"
 //!
-//!     //      "hello"
-//!     //        / \
-//!     // "world"   "trees"
-//!     //              |
-//!     //            "are"
-//!     //              |
-//!     //            "cool"
+//! let mut tree = TreeBuilder::new().with_root("hello").build();
+//! let root_id = tree.root_id().expect("root doesn't exist?");
+//! let mut hello = tree.get_mut(root_id).unwrap();
 //!
-//!     let mut tree = TreeBuilder::new().with_root("hello").build();
-//!     let root_id = tree.root_id().expect("root doesn't exist?");
-//!     let mut hello = tree.get_mut(root_id).unwrap();
-//!
-//!     hello.append("world");
-//!     hello
-//!         .append("trees")
-//!         .append("are")
-//!         .append("cool");
-//! }
+//! hello.append("world");
+//! hello
+//!     .append("trees")
+//!     .append("are")
+//!     .append("cool");
 //! ```
 //!
 //! ## `NodeId`s
